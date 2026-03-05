cb2ec44a2130821cce3a3081a27d833c0fb296d3
Andrii Chemerysov <achemerysov@gmail.com>
2026-03-04 19:04:07 -0500

    refactor most code

--- nginx/init-https.sh
+++ nginx/init-https.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-DOMAINS=(cioran.org www.cioran.org)
-EMAIL="achemerysov@gmail.com"
-DATA_PATH="./certbot"
 cd "$(dirname "$0")/.."
-if test -d "$DATA_PATH/conf/live/${DOMAINS[0]}"; then
+if test -d "./certbot/conf/live/cioran.org"; then
     echo "Existing certificates found. Skipping initialization."
     exit 0
 fi
@@ -12,8 +9,8 @@ fi
 echo "### Creating dummy certificate..."
 docker compose run --rm --entrypoint \
     "openssl req -x509 -nodes -newkey rsa:4096 -days 1 \
-    -keyout /etc/letsencrypt/live/${DOMAINS[0]}/privkey.pem \
-    -out /etc/letsencrypt/live/${DOMAINS[0]}/fullchain.pem \
+    -keyout /etc/letsencrypt/live/cioran.org/privkey.pem \
+    -out /etc/letsencrypt/live/cioran.org/fullchain.pem \
     -subj '/CN=localhost'" \
     certbot
 
@@ -23,18 +20,15 @@ docker compose -f docker-compose.yml -f docker-compose.prod.yml \
 
 echo "### Deleting dummy certificate..."
 docker compose run --rm --entrypoint \
-    "rm -rf /etc/letsencrypt/live/${DOMAINS[0]} \
-    /etc/letsencrypt/archive/${DOMAINS[0]}" \
+    "rm -rf /etc/letsencrypt/live/cioran.org \
+    /etc/letsencrypt/archive/cioran.org" \
     certbot
 
 echo "### Requesting real certificate..."
-domain_args=""
-for domain in "${DOMAINS[@]}"; do
-    domain_args="$domain_args -d $domain"
-done
 docker compose run --rm --entrypoint \
-    "certbot certonly --webroot --webroot-path=/var/www/certbot \
-    $domain_args --email $EMAIL --agree-tos --no-eff-email" \
+    "certbot certonly --webroot --webroot-path /var/www/certbot/ \
+    -d cioran.org -d www.cioran.org \
+    --email achemerysov@gmail.com --agree-tos --no-eff-email" \
     certbot
 
 echo "### Reloading nginx..."
